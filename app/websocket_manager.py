a554811106cfe537ea34b5e2a8a53beba0fb0f22
Mauro <maurohlo.123321@gmail.com>
2026-08-27 05:55:02 -0300

    Adiciona tratamento de conexões mortas no broadcast do WebSocket

--- app/websocket_manager.py
+++ app/websocket_manager.py
@@ -26,3 +26,17 @@ class ConnectionManager:
 
 
 manager = ConnectionManager()
+
+async def broadcast(self, board_id: int, message: dict):
+    if board_id not in self.active_connections:
+        return
+
+    dead_connections = []
+    for connection in self.active_connections[board_id]:
+        try:
+            await connection.send_json(message)
+        except Exception:
+            dead_connections.append(connection)
+
+    for connection in dead_connections:
+        self.disconnect(board_id, connection)
